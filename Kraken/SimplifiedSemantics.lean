ddb6cc0318023c97a2a70f68b2d8b1b044ab24a5
Andrew Ferraiuolo <aferr@google.com>
2026-05-06 10:26:53 +0000

    Separate straightline into tiers to allow the mention of an interior component in tactics

--- Kraken/SimplifiedSemantics.lean
+++ Kraken/SimplifiedSemantics.lean
@@ -30,19 +30,22 @@ def step (full_prog : List Directive) (s : MachineData) (pc : Nat) : Effects :=
         -- target_addr is already an Int64 representing the index
         .done (s', target_addr))
 
+-- Helper frunction for `straightline`. Note: keeping it as a separate def is needed
+-- to allow its use in tactics such as dsimp
+def straightline_go (full_prog : List Directive) [Labels] (ds : List Directive) (s : MachineData) (pc : Nat) : Effects :=
+  match ds with
+  | [] => .done (s, Int64.ofNat pc)
+  | d :: ds =>
+    let p : Std.Rco Int64 := .mk (Int64.ofNat pc) (Int64.ofNat (pc + 1))
+    Directive.interp d s p
+      (next := fun s' => straightline_go full_prog ds s' (pc + 1))
+      (jmp := fun target_addr s' => .done (s', target_addr))
+
 -- Straightline execution: executes instructions until the end or a jump.
 def straightline (full_prog : List Directive) (s : MachineData) (pc : Nat) : Effects :=
   let labels := mkLabels full_prog
   let _labels := labels
-  let rec go (ds : List Directive) (s : MachineData) (pc : Nat) : Effects :=
-    match ds with
-    | [] => .done (s, Int64.ofNat pc)
-    | d :: ds =>
-      let p : Std.Rco Int64 := .mk (Int64.ofNat pc) (Int64.ofNat (pc + 1))
-      Directive.interp d s p
-        (next := fun s' => go ds s' (pc + 1))
-        (jmp := fun target_addr s' => .done (s', target_addr))
-  go (full_prog.drop pc) s pc
+  straightline_go full_prog (full_prog.drop pc) s pc
 
 def simplified_step1 (prog: List Directive) (s: MachineState) (post: @Post MachineState) : Prop :=
   (step prog s.1 s.2.toInt.toNat).all post
