e6987a618892dc5211e10ff900e9cfc941a3414e
Andrew Ferraiuolo <aferr@google.com>
2026-05-19 11:26:41 +0000

    Refactor; straightline version of simplified semantics

--- Kraken/SimplifiedSemantics.lean
+++ Kraken/SimplifiedSemantics.lean
@@ -1,4 +1,5 @@
 import Kraken.Semantics
+import Kraken.Tactics
 
 namespace Kraken.Simplified
 
@@ -28,5 +29,25 @@ def step (full_prog : List Directive) (s : MachineData) (pc : Nat) : Effects :=
       (jmp := fun target_addr s' =>
         -- target_addr is already an Int64 representing the index
         .done (s', target_addr))
+
+-- Straightline execution: executes instructions until the end or a jump.
+def straightline (full_prog : List Directive) (s : MachineData) (pc : Nat) : Effects :=
+  let labels := mkLabels full_prog
+  let _labels := labels
+  let rec go (ds : List Directive) (s : MachineData) (pc : Nat) : Effects :=
+    match ds with
+    | [] => .done (s, Int64.ofNat pc)
+    | d :: ds =>
+      let p : Std.Rco Int64 := .mk (Int64.ofNat pc) (Int64.ofNat (pc + 1))
+      Directive.interp d s p
+        (next := fun s' => go ds s' (pc + 1))
+        (jmp := fun target_addr s' => .done (s', target_addr))
+  go (full_prog.drop pc) s pc
+
+def simplified_step1 (prog: List Directive) (s: MachineState) (post: @Post MachineState) : Prop :=
+  (step prog s.1 s.2.toInt.toNat).all post
+
+def simplified_straightline_step (prog: List Directive) (s: MachineState) (post: @Post MachineState) : Prop :=
+  (straightline prog s.1 s.2.toInt.toNat).all post
 
 end Kraken.Simplified
