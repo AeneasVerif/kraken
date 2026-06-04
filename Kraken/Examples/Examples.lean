fd9910856cd6199f95bfd47010e37e1399212514
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2026-06-04 13:50:22 -0700

    Review with Andres

--- Kraken/Examples/Examples.lean
+++ Kraken/Examples/Examples.lean
@@ -899,19 +899,17 @@ pop %rax")
 
 set_option maxHeartbeats 1000000
 set_option pp.rawOnError true
+/- set_option pp.coercions false -/
 /- set_option pp.all true -/
 
-theorem simpleAlignedStore64 (s : MachineData) (addr : BitVec 64) {w: Width} (v : w.type) (ret: MachineData → Effects)
+theorem simpleAlignedStore64 (s : MachineData) (addr : BitVec 64) (v : BitVec 64) (ret: MachineData → Effects)
   (hAligned: addr % 8 = 0)
-  (hContains: UInt64.ofBitVec addr ∈ s.dmem)
-  (hIs64: w = .W64):
-  have: BitVec 64 = w.type := by simp [Width.type,Width.bits,hIs64]
-  MachineData.store s addr v ret =
+  (hContains: UInt64.ofBitVec addr ∈ s.dmem):
+  @MachineData.store s addr .W64 v ret =
   require_write_access addr Width.W64 (fun _unit =>
-    ret { s with dmem := s.dmem.insert (UInt64.ofBitVec addr) (UInt64.ofBitVec (this ▸ v)) }) :=
+    ret { s with dmem := s.dmem.insert (UInt64.ofBitVec addr) (UInt64.ofBitVec v) }) :=
 by
   simp only [MachineData.store,Width.bytesv,Width.bytes]
-  subst hIs64
   have: addr % 8#64 = 0#64 := by grind
   simp only [this]
   have: UInt64.ofBitVec (addr &&& ~~~0b111#64) = UInt64.ofBitVec addr := by
@@ -919,28 +917,32 @@ by
   rw [this]
   have: addr &&& 7#64 = 0 := by bv_decide
   simp [this,Width.bits]
-  simp [BitVec.replace]
+  -- TODO: lift and generalize this
+  have (old v: BitVec 64): BitVec.replace old 0 v = v := by
+    simp [BitVec.replace,BitVec.drop]
+    bv_decide
+  simp [this]
   rw [Std.ExtHashMap.getElem?_eq_some_getElem! hContains]
-  simp
-  ext
-  simp [BitVec.drop]
-  simp [Width.type,Width.bits] at v
-  have : 0#0 ++ v = v := by bv_decide
-  rw [this]
 
-theorem simpleAlignedLoad64 (s : MachineData) (addr : BitVec 64) {w: Width} (ret: w.type → MachineData → Effects)
+-- AE, JP: it would be nice to eliminate CPS from the post and say that the post
+-- is just called with the value that is loaded
+theorem simpleAlignedLoad64 (s : MachineData) (addr : BitVec 64) (ret: BitVec 64 → MachineData → Effects)
   (hAligned: addr % 8 = 0)
-  (hContains: UInt64.ofBitVec addr ∈ s.dmem)
-  (hIs64: w = .W64):
-  have: w.type = BitVec 64 := by simp [Width.type,Width.bits,hIs64]
-  MachineData.load s addr w ret =
+  (hContains: UInt64.ofBitVec addr ∈ s.dmem):
+  MachineData.load s addr .W64 ret =
   require_read_access addr Width.W64 (fun _unit =>
-    ret (this ▸ (s.dmem[UInt64.ofBitVec addr]!).toBitVec) s) :=
+    ret (s.dmem[UInt64.ofBitVec addr]!.toBitVec) s) :=
 by
   simp only [MachineData.load,Width.bytesv,Width.bytes]
-  subst hIs64
+  -- TODO: figure out why `simp` cannot deduce the fact below; setting
+  -- pp.coercions to false reveals that one version of the lemma uses
+  -- `OfNat.ofNat` to produce a BitVec, while the other uses `OfNat.ofNat` to
+  -- produce a Nat, followed by a call to `BitVec.ofNat`, and there appears to
+  -- be no simp-rule that would show that one can be rewritten into the other.
   have: addr % 8#64 = 0#64 := by grind
   simp only [this]
+  -- TODO: perhaps there should be a rule for this that can be in a simpset or a
+  -- grindset to facilitate this sort of reasoning
   have: UInt64.ofBitVec (addr &&& ~~~0b111#64) = UInt64.ofBitVec addr := by
     bv_decide
   rw [this]
@@ -949,7 +951,7 @@ by
   rw [Std.ExtHashMap.getElem?_eq_some_getElem! hContains]
 
 example [layout : Layout] (s: MachineData)
-  (hAlign: s.regs.rsp % (8 : UInt64) = 0)
+  (hAlign: s.regs.rsp % 8 = 0)
   (hContains: forall x, x ∈ s.dmem)
   : Step1 (layout p6) (s, layout.start) (fun s' => s'.1.regs.rax = s.regs.rax) := by
   -- Refine the state to make registers apparent -- note that `cases` consumes
